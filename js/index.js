a627412861c17156c8cbd0e4da51b382545006ed
Ribeiro1300 <59618809+Ribeiro1300@users.noreply.github.com>
2021-06-28 11:08:19 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ var buttonColors = ["red", "blue", "green","yellow"]
 var gamePattern = []
 var userClickedPattern = []
 var level = 0
+var started = false
 
 //som dos botões
 function playSound(name)
@@ -20,15 +21,15 @@ function animatePress(currentColor)
 
 }
 
-function nextSequence()
+$("#level-title").click(function ()
 {
-    var randomN = Math.floor(Math.random()*4)
-    playSound(buttonColors[randomN])
-    var randomChosenColor = buttonColors[randomN]
-    gamePattern.push(randomChosenColor)
-    $("h1").text("Level "+level)
-    level++
-}
+    if (started != true)
+    {
+        $("h1").text("Level "+level)
+        nextSequence();
+        started = true;
+    }
+})
 
 //Escolha do jogador
 $(".btn").click(function(event)
@@ -37,6 +38,49 @@ $(".btn").click(function(event)
     animatePress(userChosenColor)
     playSound(userChosenColor)
     userClickedPattern.push(userChosenColor)
+    if (gamePattern.length == userClickedPattern.length)
+        checkAnswer(userClickedPattern.length - 1)
 })
 
-$("h1").click(nextSequence())
+
+function checkAnswer(currentLevel)
+{
+
+    if (gamePattern[currentLevel] == userClickedPattern[currentLevel])
+    {
+        console.log("sucess")
+
+        if (gamePattern.length == userClickedPattern.length)
+            setTimeout(nextSequence(), 100000)
+    }
+    else
+    {
+        console.log("wrong")
+        $("body").addClass("game-over")
+        setTimeout(function()
+        {
+            $("body").removeClass("game-over")
+        },100)
+        $("h1").text("Game Over, Press Any Key to Restart")
+        startOver()
+    }
+    
+    
+}
+function nextSequence()
+{
+    var randomN = Math.floor(Math.random()*4)
+    playSound(buttonColors[randomN])
+    animatePress(buttonColors[randomN])
+    var randomChosenColor = buttonColors[randomN]
+    gamePattern.push(randomChosenColor)
+    $("h1").text("Level "+level)
+    level++
+    userClickedPattern = []
+}
+function startOver()
+{
+    level = 0
+    gamePattern = []
+    started = false
+}
